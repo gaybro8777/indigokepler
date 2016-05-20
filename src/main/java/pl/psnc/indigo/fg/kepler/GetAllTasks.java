8c41ecf5b1b5100462ed65c437b25c3042776c5b
Tomasz Żok <tomasz.zok@cs.put.poznan.pl>
2016-05-20 16:46:17 +0200

    Use the recent changed API from indigoclient

--- src/main/java/pl/psnc/indigo/fg/kepler/GetAllTasks.java
+++ src/main/java/pl/psnc/indigo/fg/kepler/GetAllTasks.java
@@ -1,66 +1,70 @@
 package pl.psnc.indigo.fg.kepler;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import pl.psnc.indigo.fg.api.restful.BaseAPI;
+import pl.psnc.indigo.fg.api.restful.TasksAPI;
+import pl.psnc.indigo.fg.api.restful.exceptions.FutureGatewayException;
+import pl.psnc.indigo.fg.api.restful.jaxb.Task;
+import ptolemy.actor.TypedIOPort;
 import ptolemy.actor.lib.LimitedFiringSource;
 import ptolemy.data.StringToken;
-import ptolemy.data.expr.Parameter;
 import ptolemy.data.type.BaseType;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
-import ptolemy.actor.TypedIOPort;
 import ptolemy.kernel.util.SingletonAttribute;
-import pl.psnc.indigo.fg.api.restful.TasksAPI;
-import pl.psnc.indigo.fg.api.restful.BaseAPI;
-import pl.psnc.indigo.fg.api.restful.jaxb.Task;
+
+import java.util.List;
 
 public class GetAllTasks extends LimitedFiringSource {
 
-  public TypedIOPort userPort;
+    public TypedIOPort userPort;
 
-  public GetAllTasks(CompositeEntity container, String name)
-    throws NameDuplicationException, IllegalActionException {
-    super(container, name);
+    public GetAllTasks(CompositeEntity container, String name)
+            throws NameDuplicationException, IllegalActionException {
+        super(container, name);
 
-    userPort = new TypedIOPort(this, "user", true, false);
-    new SingletonAttribute(userPort, "_showName");
-    userPort.setTypeEquals(BaseType.STRING);
+        userPort = new TypedIOPort(this, "user", true, false);
+        new SingletonAttribute(userPort, "_showName");
+        userPort.setTypeEquals(BaseType.STRING);
 
-    output.setTypeEquals(BaseType.STRING);
-  }
-
-  @Override
-  public void fire() throws IllegalActionException {
-    super.fire();
-
-    String userString = null;
-
-    if (userPort.getWidth() > 0) {
-      StringToken userToken = (StringToken) userPort.get(0);
-      userString = userToken.stringValue();
+        output.setTypeEquals(BaseType.STRING);
     }
 
-    TasksAPI restAPI = new TasksAPI(BaseAPI.LOCALHOST_ADDRESS);
+    @Override
+    public void fire() throws IllegalActionException {
+        super.fire();
 
-    try {
-      Task taskToGet = new Task();
-      taskToGet.setUser(userString);
-      Task[] result = restAPI.getAllTasks();
+        String userString = null;
 
-      JSONArray array = new JSONArray();
+        if (userPort.getWidth() > 0) {
+            StringToken userToken = (StringToken) userPort.get(0);
+            userString = userToken.stringValue();
+        }
 
-      for (Task task : result) {
-        JSONObject object = new JSONObject();
-        object.put(task.getId(), task.getStatus());
-        array.put(object);
-      }
+        Task taskToGet = new Task();
+        taskToGet.setUser(userString);
 
-      String jsonString = array.toString();
+        JSONArray array;
+        try {
+            TasksAPI restAPI = new TasksAPI(BaseAPI.LOCALHOST_ADDRESS);
+            List<Task> result = restAPI.getAllTasks();
 
-      output.send(0, new StringToken(jsonString));
-    } catch (Exception ex) {
-      throw new IllegalActionException("There was an issue during task submission");
+            array = new JSONArray();
+
+            for (Task task : result) {
+                JSONObject object = new JSONObject();
+                object.put(task.getId(), task.getStatus());
+                array.put(object);
+            }
+        } catch (FutureGatewayException e) {
+            throw new IllegalActionException(this, e, "Failed to get all tasks");
+        } catch (JSONException e) {
+            throw new IllegalActionException(this, e, "Failed to get all tasks");
+        }
+
+        output.send(0, new StringToken(array.toString()));
     }
-  }
 }
--- src/main/java/pl/psnc/indigo/fg/kepler/GetTask.java
+++ src/main/java/pl/psnc/indigo/fg/kepler/GetTask.java
@@ -1,74 +1,72 @@
 package pl.psnc.indigo.fg.kepler;
 
-import java.util.logging.Logger;
+import pl.psnc.indigo.fg.api.restful.BaseAPI;
+import pl.psnc.indigo.fg.api.restful.TasksAPI;
+import pl.psnc.indigo.fg.api.restful.exceptions.FutureGatewayException;
+import pl.psnc.indigo.fg.api.restful.jaxb.Task;
+import ptolemy.actor.TypedIOPort;
 import ptolemy.actor.lib.LimitedFiringSource;
 import ptolemy.data.StringToken;
-import ptolemy.data.expr.Parameter;
 import ptolemy.data.type.BaseType;
 import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
-import ptolemy.actor.TypedIOPort;
 import ptolemy.kernel.util.SingletonAttribute;
-import pl.psnc.indigo.fg.api.restful.TasksAPI;
-import pl.psnc.indigo.fg.api.restful.BaseAPI;
-import pl.psnc.indigo.fg.api.restful.jaxb.Task;
 
 public class GetTask extends LimitedFiringSource {
+    public TypedIOPort userPort;
+    public TypedIOPort idPort;
+    public TypedIOPort statusPort;
 
-  private final static Logger LOGGER = Logger.getLogger(GetTask.class.getName());
+    public GetTask(CompositeEntity container, String name)
+            throws NameDuplicationException, IllegalActionException {
+        super(container, name);
 
-  public TypedIOPort userPort;
-  public TypedIOPort idPort;
-  public TypedIOPort statusPort;
+        userPort = new TypedIOPort(this, "user", true, false);
+        new SingletonAttribute(userPort, "_showName");
+        userPort.setTypeEquals(BaseType.STRING);
 
-  public GetTask(CompositeEntity container, String name)
-    throws NameDuplicationException, IllegalActionException {
-    super(container, name);
+        idPort = new TypedIOPort(this, "id", true, false);
+        new SingletonAttribute(idPort, "_showName");
+        idPort.setTypeEquals(BaseType.STRING);
 
-    userPort = new TypedIOPort(this, "user", true, false);
-    new SingletonAttribute(userPort, "_showName");
-    userPort.setTypeEquals(BaseType.STRING);
+        statusPort = new TypedIOPort(this, "status", false, true);
+        new SingletonAttribute(statusPort, "_showName");
+        statusPort.setTypeEquals(BaseType.STRING);
 
-    idPort = new TypedIOPort(this, "id", true, false);
-    new SingletonAttribute(idPort, "_showName");
-    idPort.setTypeEquals(BaseType.STRING);
-
-    statusPort = new TypedIOPort(this, "status", false, true);
-    new SingletonAttribute(statusPort, "_showName");
-    statusPort.setTypeEquals(BaseType.STRING);
-
-    output.setTypeEquals(BaseType.STRING);
-  }
-
-  @Override
-  public void fire() throws IllegalActionException {
-    super.fire();
-
-    String userString = null;
-    String idString = null;
-
-    if (userPort.getWidth() > 0) {
-      StringToken userToken = (StringToken) userPort.get(0);
-      userString = userToken.stringValue();
+        output.setTypeEquals(BaseType.STRING);
     }
 
-    if (idPort.getWidth() > 0) {
-      StringToken idToken = (StringToken) idPort.get(0);
-      idString = idToken.stringValue();
-    }
+    @Override
+    public void fire() throws IllegalActionException {
+        super.fire();
 
-    TasksAPI restAPI = new TasksAPI(BaseAPI.LOCALHOST_ADDRESS);
+        String userString = null;
+        String idString = null;
 
-    try {
-      Task taskToGet = new Task();
-      taskToGet.setUser(userString);
-      taskToGet.setId(idString);
-      Task result = restAPI.getTask(taskToGet);
-      output.send(0, new StringToken(result.getId()));
-      statusPort.send(0, new StringToken(result.getStatus()));
-    } catch (Exception ex) {
-      throw new IllegalActionException(this, ex, "There was an issue while checking task status");
+        if (userPort.getWidth() > 0) {
+            StringToken userToken = (StringToken) userPort.get(0);
+            userString = userToken.stringValue();
+        }
+
+        if (idPort.getWidth() > 0) {
+            StringToken idToken = (StringToken) idPort.get(0);
+            idString = idToken.stringValue();
+        }
+
+        Task taskToGet = new Task();
+        taskToGet.setUser(userString);
+        taskToGet.setId(idString);
+
+        Task result = null;
+        try {
+            TasksAPI restAPI = new TasksAPI(BaseAPI.LOCALHOST_ADDRESS);
+            result = restAPI.getTask(taskToGet);
+        } catch (FutureGatewayException e) {
+            throw new IllegalActionException(this, e, "Failed to get task");
+        }
+
+        output.send(0, new StringToken(result.getId()));
+        statusPort.send(0, new StringToken(result.getStatus().name()));
     }
-  }
 }
