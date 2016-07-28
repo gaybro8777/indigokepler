829951187ce4832c83df7e18b5f37ab9fd01d8cd
Tomasz Żok <tomasz.zok@cs.put.poznan.pl>
2016-07-28 14:00:54 +0200

    Update Ophidia use case

---
+++ src/main/java/pl/psnc/indigo/fg/kepler/ophidia/ParseOutputJson.java
@@ -0,0 +1,96 @@
+package pl.psnc.indigo.fg.kepler.ophidia;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import pl.psnc.indigo.fg.kepler.helper.AllowedPublicField;
+import pl.psnc.indigo.fg.kepler.helper.PortHelper;
+import ptolemy.actor.TypedIOPort;
+import ptolemy.actor.lib.LimitedFiringSource;
+import ptolemy.data.StringToken;
+import ptolemy.data.type.BaseType;
+import ptolemy.kernel.CompositeEntity;
+import ptolemy.kernel.util.IllegalActionException;
+import ptolemy.kernel.util.NameDuplicationException;
+import ptolemy.kernel.util.SingletonAttribute;
+
+import javax.ws.rs.core.UriBuilder;
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Actor to parse JSON produced by Ophidia workflow and generate URI of results.
+ */
+public class ParseOutputJson extends LimitedFiringSource {
+    private static final String JOB_ID = "JobID";
+    private static final String SESSION_CODE = "Session Code";
+    private static final String WORKFLOW = "Workflow";
+    private static final int DEFAULT_PORT = 8080;
+    /**
+     * Port to receive contents of JSON file from Ophidia.
+     */
+    @AllowedPublicField
+    public TypedIOPort jsonPort;
+
+    public ParseOutputJson(final CompositeEntity container, final String name)
+            throws NameDuplicationException, IllegalActionException {
+        super(container, name);
+
+        jsonPort = new TypedIOPort(this, "json", true, false);
+        jsonPort.setTypeEquals(BaseType.STRING);
+        new SingletonAttribute(jsonPort, "_showName");
+
+        output.setName("resultsUri");
+        output.setTypeEquals(BaseType.STRING);
+        new SingletonAttribute(output, "_showName");
+    }
+
+    @Override
+    public final void fire() throws IllegalActionException {
+        super.fire();
+
+        String json = PortHelper.readStringMandatory(jsonPort);
+
+        String[] keys;
+        String[] values;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode jsonNode = mapper.readTree(json);
+            jsonNode = jsonNode.get("response");
+            jsonNode = jsonNode.get("source");
+            keys = mapper.treeToValue(jsonNode.get("keys"), String[].class);
+            values = mapper.treeToValue(jsonNode.get("values"), String[].class);
+        } catch (IOException e) {
+            throw new IllegalActionException(this, e, "Failed to parse JSON");
+        }
+
+        if (keys.length != values.length) {
+            throw new IllegalActionException(this,
+                                             "Invalid JSON: keys and values "
+                                             + "do not match");
+        }
+
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < keys.length; i++) {
+            map.put(keys[i], values[i]);
+        }
+
+        if (!map.containsKey(ParseOutputJson.JOB_ID) || !map
+                .containsKey(ParseOutputJson.SESSION_CODE) || !map
+                .containsKey(ParseOutputJson.WORKFLOW)) {
+            throw new IllegalActionException(this,
+                                             "Invalid JSON: lack of expected "
+                                             + "keys");
+        }
+
+        String sessionCode = map.get(ParseOutputJson.SESSION_CODE);
+        String workflowId = map.get(ParseOutputJson.WORKFLOW);
+        URI uri = UriBuilder.fromUri(map.get(ParseOutputJson.JOB_ID))
+                            .replacePath("").port(ParseOutputJson.DEFAULT_PORT)
+                            .path("/thredds/dodsC/indigo/precip_trend_input/")
+                            .path(sessionCode).path(workflowId)
+                            .path("precip_trend_analysis.nc").build();
+        output.broadcast(new StringToken(uri.toString()));
+    }
+}
--- workflows/ophidia/fg.sql
+++ workflows/ophidia/fg.sql
@@ -14,3 +14,20 @@ INSERT INTO application_parameter VALUES(100, 4, 'jobdesc_error', 'stderr.txt', 
 INSERT INTO application_parameter VALUES(100, 5, 'target_executor', 'GridEngine', NULL);
 
 INSERT INTO infrastructure VALUES(1, 100, 'ophidia on localhost', 'Run oph_term as jobtest@localhost', NOW(), 1, 0);
+
+
+-- Application with id 101 is *almost* identical. It only has one more input file: arguments.txt
+INSERT INTO application VALUES(101, 'ophidia-localhost-input-file', 'Run Ophidia terminal from localhost + read arguments from input file', 'JOB', NOW(), 1);
+
+INSERT INTO application_file VALUES(101, 1, 'script.sh', '/home/futuregateway/FutureGateway/fgAPIServer/apps/ophidia-localhost', 0);
+INSERT INTO application_file VALUES(101, 2, 'workflow.json', '/home/futuregateway/FutureGateway/fgAPIServer/apps/ophidia-localhost', 0);
+INSERT INTO application_file VALUES(101, 3, 'oph-credentials.txt', '/home/futuregateway/FutureGateway/fgAPIServer/apps/ophidia-localhost', 0);
+INSERT INTO application_file VALUES(101, 4, 'arguments.txt', '/home/futuregateway/FutureGateway/fgAPIServer/apps/ophidia-localhost', 0);
+
+INSERT INTO application_parameter VALUES(101, 1, 'jobdesc_executable', '/bin/bash', NULL);
+INSERT INTO application_parameter VALUES(101, 2, 'jobdesc_arguments', 'script.sh', NULL);
+INSERT INTO application_parameter VALUES(101, 3, 'jobdesc_output', 'stdout.txt', NULL);
+INSERT INTO application_parameter VALUES(101, 4, 'jobdesc_error', 'stderr.txt', NULL);
+INSERT INTO application_parameter VALUES(101, 5, 'target_executor', 'GridEngine', NULL);
+
+INSERT INTO infrastructure VALUES(1, 101, 'ophidia on localhost', 'Run oph_term as jobtest@localhost', NOW(), 1, 0);
