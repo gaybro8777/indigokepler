717d3629ff664096989a6e1651c06ed93cafc6e1
Tomasz Żok <tomasz.zok@cs.put.poznan.pl>
2017-08-02 14:56:18 +0200

    Provide automatic token refresh functionality

--- src/main/java/pl/psnc/indigo/fg/kepler/FutureGatewayActor.java
+++ src/main/java/pl/psnc/indigo/fg/kepler/FutureGatewayActor.java
@@ -1,6 +1,13 @@
 package pl.psnc.indigo.fg.kepler;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.psnc.indigo.fg.api.restful.RootAPI;
+import pl.psnc.indigo.fg.api.restful.TokenHelper;
+import pl.psnc.indigo.fg.api.restful.exceptions.FutureGatewayException;
 import pl.psnc.indigo.fg.kepler.helper.PortHelper;
 import ptolemy.actor.TypedIOPort;
 import ptolemy.actor.lib.LimitedFiringSource;
@@ -11,11 +18,22 @@ import ptolemy.kernel.CompositeEntity;
 import ptolemy.kernel.util.IllegalActionException;
 import ptolemy.kernel.util.NameDuplicationException;
 
+import java.net.URI;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.concurrent.TimeUnit;
+
 /**
  * An actor which reads desired Future Gateway URI in the beginning
  * of its operation.
  */
 public class FutureGatewayActor extends LimitedFiringSource {
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(FutureGatewayActor.class);
+    private static final String DEFAULT_TOKEN_SERVICE_URI =
+            "https://fgw01.ncg.ingrid.pt/api/jsonws/iam.token/get-token";
+
     /**
      * Port for URI of a Future Gateway instance.
      */
@@ -32,9 +50,21 @@ public class FutureGatewayActor extends LimitedFiringSource {
      * Parameter for authorization token from the user.
      */
     private final StringParameter authorizationToken;
+    /**
+     * Parameter for token webservice URI, part of indigo-dc/LiferayPlugins
+     */
+    private final StringParameter tokenServiceUri;
+    /**
+     * Parameter for token webservice username, part of indigo-dc/LiferayPlugins
+     */
+    private final StringParameter tokenServiceUser;
+    /**
+     * Parameter for token webservice password, part of indigo-dc/LiferayPlugins
+     */
+    private final StringParameter tokenServicePassword;
 
-    public FutureGatewayActor(
-            final CompositeEntity container, final String name)
+    public FutureGatewayActor(final CompositeEntity container,
+                              final String name)
             throws NameDuplicationException, IllegalActionException {
         super(container, name);
 
@@ -55,14 +85,24 @@ public class FutureGatewayActor extends LimitedFiringSource {
                 new StringParameter(this, "authorizationToken"); //NON-NLS
         authorizationToken.setToken("");
 
+        tokenServiceUri = new StringParameter(this, "tokenServiceUri");
+        tokenServiceUri.setToken(FutureGatewayActor.DEFAULT_TOKEN_SERVICE_URI);
+
+        tokenServiceUser = new StringParameter(this, "tokenServiceUser");
+        tokenServiceUser.setToken("");
+
+        tokenServicePassword =
+                new StringParameter(this, "tokenServicePassword");
+        tokenServicePassword.setToken("");
+
         PortHelper.makePortNameVisible(futureGatewayUriPort,
                                        authorizationTokenPort);
     }
 
     protected final String getFutureGatewayUri() throws IllegalActionException {
-        if ((futureGatewayUriPort.getWidth() > 0) && futureGatewayUriPort
-                .hasToken(0)) {
-            Token token = futureGatewayUriPort.get(0);
+        if ((futureGatewayUriPort.getWidth() > 0) &&
+            futureGatewayUriPort.hasToken(0)) {
+            final Token token = futureGatewayUriPort.get(0);
             futureGatewayUri.setToken(token);
         }
         return futureGatewayUri.stringValue();
@@ -70,11 +110,66 @@ public class FutureGatewayActor extends LimitedFiringSource {
 
     protected final String getAuthorizationToken()
             throws IllegalActionException {
-        if ((authorizationTokenPort.getWidth() > 0) && authorizationTokenPort
-                .hasToken(0)) {
-            Token token = authorizationTokenPort.get(0);
+        if ((authorizationTokenPort.getWidth() > 0) &&
+            authorizationTokenPort.hasToken(0)) {
+            final Token token = authorizationTokenPort.get(0);
             authorizationToken.setToken(token);
         }
+
+        final DecodedJWT jwt = decodeToken(authorizationToken.stringValue());
+        final long expiresAt = jwt.getExpiresAt().getTime();
+        final long now = System.currentTimeMillis();
+        final long diff = expiresAt - now;
+        final long minutes = TimeUnit.MILLISECONDS.toMinutes(diff);
+
+        final LocalDateTime nowLocalDateTime =
+                Instant.ofEpochMilli(now).atZone(ZoneId.systemDefault())
+                       .toLocalDateTime();
+        final LocalDateTime expiresAtLocalDateTime =
+                Instant.ofEpochMilli(expiresAt).atZone(ZoneId.systemDefault())
+                       .toLocalDateTime();
+        FutureGatewayActor.LOGGER
+                .info("Now is {}. IAM token expires at {}. The difference is " +
+                      "{} minutes.", nowLocalDateTime, expiresAtLocalDateTime,
+                      minutes);
+
+        // 3 minutes is taken from indigo-dc/LiferayPlugins
+        // this is the duration, below which it uses refresh token
+        if (minutes < 3) {
+            final String token = refreshToken();
+            authorizationToken.setToken(token);
+        }
+
         return authorizationToken.stringValue();
+    }
+
+    private String refreshToken() throws IllegalActionException {
+        try {
+            final String token = TokenHelper
+                    .getToken(authorizationToken.stringValue(),
+                              URI.create(tokenServiceUri.stringValue()),
+                              tokenServiceUser.stringValue(),
+                              tokenServicePassword.stringValue());
+            final DecodedJWT newJwt = decodeToken(token);
+            final LocalDateTime newExpiresAt = LocalDateTime
+                    .ofInstant(newJwt.getExpiresAt().toInstant(),
+                               ZoneId.systemDefault());
+            FutureGatewayActor.LOGGER
+                    .info("IAM token refreshed, expires at {}", newExpiresAt);
+            return token;
+        } catch (final FutureGatewayException e) {
+            throw new IllegalActionException(this, e,
+                                             "Failed to refresh token");
+        }
+    }
+
+    private DecodedJWT decodeToken(final String token)
+            throws IllegalActionException {
+        try {
+            return JWT.decode(token);
+        } catch (final JWTDecodeException e) {
+            throw new IllegalActionException(this, e,
+                                             "Invalid authorization token");
+        }
     }
 }
